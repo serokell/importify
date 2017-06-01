30c317c91ff1b55225b5a21caf05eea77c454e2e
Dmitry Kovanikov <kovanikov@gmail.com>
2017-06-01 16:23:40 +0300

    [IMRF-7] Add base package modules to tests
    Now only imports from `base` package are working. So I've added modules
    from that package to test things. Though, `Bool` in _With_ test is not working.
    But that's not urgent now.

--- test/BaseThingWithExample.hs
+++ test/BaseThingWithExample.hs
@@ -1,7 +1,8 @@
 module BaseThingWithExample where
 
+import           Data.Bool             (Bool (False, True))
 import           Language.Haskell.Exts (DataOrNew (DataType))
-import           System.IO (putStrLn)
+import           System.IO             (putStrLn)
 
 main :: IO ()
-main = putStrLn "Hello!"
+main = putStrLn $ "Hello!" ++ show True
