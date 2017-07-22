f7704356570ce60bebda17979c61b44af2b7ad42
Dmitry Kovanikov <kovanikov@gmail.com>
2017-07-22 20:34:04 +0300

    :fire_engine: :umbrella: [IMRF-76] Keep exported imports

---
+++ test/test-data/base:basic/08-Z05-HidingListWhenExportedAs.hs
@@ -0,0 +1,4 @@
+-- TODO: behaviour should be different!
+module HidingRemovedWhenExported (module L) where
+
+import           Data.List as L hiding (sort)
---
+++ test/test-data/base:basic/36-ExportModuleImplicit.hs
@@ -0,0 +1,3 @@
+module ExportModuleImplicit (module Data.List) where
+
+import           Data.List
